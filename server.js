d28071fe78774251957f0fa1f74d3cca56d6d769
Gieryneck <gierynt@gmail.com>
2018-01-02 14:07:32 +0100

    Zadanie3. Middleware for '/store' endpoint only.

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
 
 
 app.get('/', function(req, res) {
     console.log('Received GET request at "/".');
-    res.sendFile('/index.html');
+    res.send('Hello there!');
 });
 
+app.use('/store',function(req, res, next) { // middleware for "/store" endpoint only
+    console.log('I\'m middleware for request at "/store".');
+    next();
+});
 
-app.get('/userform', function(req, res) {
-    console.log('Received GET request at "/userform".');
-    const response = {
+app.get('/store', function(req, res){
 
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response)); // zakoncz odpowiedz, ale jeszcze przetworz const response na jsonowy string
-    /* 
-    res.write('<h1>It works!</h1>', 'utf8');
-    res.end();
-
-    is equivalent to
-
-    res.end('<h1>It works!</h1>', 'utf8');
-     */
+    console.log('Received GET request at "/store".');
+    res.send('This is the store.');
 });
 
 
@@ -42,3 +33,4 @@ var server = app.listen(3000, 'localhost', function() {
 app.use(function (req, res, next) {
     res.status(404).send('Sorry, couldn\'t find what you were looking for.');
 });
+
